8ed5b6f5cca349fc0d76dc49d3893a3ac132c9b3
Haein Kim <hayni100@gmail.com>
2023-03-16 18:54:21 -0700

    income values return

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
-import Detail from './pages/Detail';
+// import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -18,8 +18,8 @@ import Expense from './pages/Expense'
 import Nav from './components/Nav';
 
 import { StoreProvider } from './utils/GlobalState';
-import Success from './pages/Success';
-import OrderHistory from './pages/OrderHistory';
+// import Success from './pages/Success';
+// import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -59,18 +59,6 @@ function App() {
               <Route 
                 path="/signup" 
                 element={<Signup />} 
-              />
-              <Route 
-                path="/success" 
-                element={<Success />} 
-              />
-              <Route 
-                path="/orderHistory" 
-                element={<OrderHistory />} 
-              />
-              <Route 
-                path="/products/:id" 
-                element={<Detail />} 
               />
               <Route
               path="/income"
