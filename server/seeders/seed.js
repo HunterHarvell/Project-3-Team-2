264ab70c4c524a5e302097bb6821c7a44b5571cd
Genevieve Ferrari <genferrari@gmail.com>
2023-03-21 22:35:51 -0700

    fixed code

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,31 +1,31 @@
-const db = require("../config/connection");
-const { User, Income } = require("../models");
-const userSeeds = require("./userSeeds.json");
-const IncomeSeeds = require("./incomeSeeds.json");
+// const db = require("../config/connection");
+// const { User, Income } = require("../models");
+// const userSeeds = require("./userSeeds.json");
+// const IncomeSeeds = require("./incomeSeeds.json");
 
-db.once("open", async () => {
-  try {
-    await Income.deleteMany({});
-    await User.deleteMany({});
+// db.once("open", async () => {
+//   try {
+//     await Income.deleteMany({});
+//     await User.deleteMany({});
 
-    await User.create(userSeeds);
+//     await User.create(userSeeds);
 
-    for (let i = 0; i < IncomeSeeds.length; i++) {
-      const { text, amount } = await Income.create(IncomeSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { income: text },
-        {
-          $addToSet: {
-            income: text,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+//     for (let i = 0; i < IncomeSeeds.length; i++) {
+//       const { text, amount } = await Income.create(IncomeSeeds[i]);
+//       const user = await User.findOneAndUpdate(
+//         { income: text },
+//         {
+//           $addToSet: {
+//             income: text,
+//           },
+//         }
+//       );
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
 
-  console.log("all done!");
-  process.exit(0);
-});
+//   console.log("all done!");
+//   process.exit(0);
+// });
--- server/server.js
+++ server/server.js
@@ -32,9 +32,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+//THIS IS FOR DEPLOYED APP!!!!!!
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
