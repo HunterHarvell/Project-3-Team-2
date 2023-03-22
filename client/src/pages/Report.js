0c07ceced39f0c3653b92b12f2a4f7d614e0d3fb
Genevieve Ferrari <genferrari@gmail.com>
2023-03-22 12:03:54 -0700

    added old report js code

---
+++ client/src/pages/Report.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReportCard from "../components/ReportCard/index";
+const Report = () => {
+  return (
+    <div className="container">
+      <h1 style={{ padding: 20 }}>Reports</h1>
+      <ReportCard />
+    </div>
+  );
+};
+
+export default Report;
