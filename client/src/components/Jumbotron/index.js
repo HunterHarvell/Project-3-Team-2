8bb7dbc401b6f99420e7319375f9fc807d440403
Genevieve Ferrari <genferrari@gmail.com>
2023-03-23 12:07:07 -0700

    adding jumbotron js

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import Auth from "../../utils/auth";
+// import Card from '@mui/material/Card';
+// import CardActions from '@mui/material/CardActions';
+// import CardContent from '@mui/material/CardContent';
+// import CardMedia from '@mui/material/CardMedia';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
 
 function Jumbotron({ children }) {
-  return (
-    <div
-      style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
-    >
-      {children}
-    </div>
-  );
+  function loginJumbotron() {
+    if (Auth.loggedIn()) {
+      return (
+        // <div class="container"></div>
+        <div
+          style={{
+            height: 560,
+            clear: "both",
+            paddingTop: 120,
+            textAlign: "center",
+          }}
+        >
+          <h1>
+            {/* Hello {$firstName}! Wecome to your {$businessName} Page! */}
+          </h1>
+
+          {children}
+        </div>
+      );
+    } else {
+      return (
+        <div
+          type="grod"
+          style={{
+            height: 560,
+            clear: "both",
+            paddingTop: 120,
+            textAlign: "center",
+            background: "#ffc34d",
+            borderColor: "#ffc34d",
+          }}
+        >
+          <ul className="flex-row">
+            <ul className="mx-1">
+              <card>
+                <p>Welcome! We're so glad you're here</p>
+              </card>
+            </ul>
+          </ul>
+        </div>
+      );
+    }
+  }
+  return <div>{loginJumbotron()}</div>;
 }
 
 export default Jumbotron;
